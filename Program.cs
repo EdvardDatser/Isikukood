592ba95dc6d1eb6e4de94a12532ab01959014e72
opilane <opilane@HP-CZC2349HT2>
2023-09-12 16:32:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.Emit;
+
+namespace OOPisik
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Sisesta isikukood: ");
+                string id = Console.ReadLine();
+                IdCode idcode = new IdCode(id);
+                Console.WriteLine(id);
+                //Console.Clear();
+                //IdCode.Menu();
+                //if (idcode.IsValid())
+                //{
+                //    Menu();
+
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Smth wrong");
+                //}
+
+            }
+        }
+    }
+}
